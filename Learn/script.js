d2745ffcaf5292b625ca46b23d3ed8cbd2f063e5
jaylearnjs <jaylearnjs@gmail.com>
2022-02-05 23:26:44 +0530

    Values and Variables

--- Learn/script.js
+++ Learn/script.js
@@ -1,3 +1,25 @@
 let js = "Amazing";
-if (js === "Amazing") alert("JavaScript is " + js);
-console.log(10 + 50 + 9 - 3);
+console.log(40 + 50 + 9 - 3);
+
+console.log("Abcd");
+console.log(44);
+
+let firstName = "Jay";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jay_patel = "jp";
+let $function = 89;
+
+let person = "Langoor";
+let PI = 3.1415;
+
+let myFirstJob = "Learner";
+let myCurrentJob = "Developer";
+
+console.log(myCurrentJob);
+
+
+ 
