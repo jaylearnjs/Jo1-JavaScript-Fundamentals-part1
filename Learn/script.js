5fa47e83bfdf92cdd57fe5a764912ab33a2de689
jaylearnjs <jaylearnjs@gmail.com>
2022-02-08 23:20:55 +0530

    Type Conversion and Coercion

--- Learn/script.js
+++ Learn/script.js
@@ -201,7 +201,7 @@ HINT: Use an if/else statement 😉
 
 GOOD LUCK 😀
 */
-
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -221,3 +221,24 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
 }
+*/
+
+//Type Conversion and Coercion
+// Type Conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type Coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n); 
