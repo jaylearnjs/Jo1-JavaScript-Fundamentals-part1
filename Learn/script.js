29af0788ff04b28440545aef7a7bdafd13ca6463
jaylearnjs <jaylearnjs@gmail.com>
2022-02-09 23:16:37 +0530

    The switch Statement

--- Learn/script.js
+++ Learn/script.js
@@ -337,7 +337,6 @@ TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 GOOD LUCK 😀
 */
 
-
 // const scoreDolphins = (96 + 108 + 89) / 3;
 // const scoreKoalas = (88 + 91 + 110) / 3;
 // console.log(scoreDolphins, scoreKoalas);
@@ -349,7 +348,7 @@ GOOD LUCK 😀
 // } else if (scoreDolphins === scoreKoalas) {
 //   console.log('Both win the trophy!');
 // }
-
+/*
 // BONUS 1
 const scoreDolphins = (97 + 112 + 80) / 3;
 const scoreKoalas = (109 + 95 + 50) / 3;
@@ -363,4 +362,46 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log('Both win the trophy!');
 } else {
   console.log('No one wins the trophy 😭');
+}
+*/
+
+// The switch Statement
+const day = 'friday';
+
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
 }
